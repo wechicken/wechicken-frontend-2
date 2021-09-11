f070e194eabd412649c746e1f39d9903c9862c00
guswnl0610 <guswnl0610@naver.com>
2021-09-11 17:02:26 +0900

    feat: joingroup 추가, css 조금 변경

--- library/api/mygroup.ts
+++ library/api/mygroup.ts
@@ -4,24 +4,28 @@ import { TEST_TOKEN } from 'library/constants';
 import { Obj } from 'library/models';
 import { apiClient } from './apiClient';
 
-export const getMyGroup = (): Promise<MyGroup> => {
+export const getMyGroup = (token = ''): Promise<MyGroup> => {
   return apiClient
-    .get<MyGroup>(`/mygroup`, { headers: { Authorization: TEST_TOKEN } })
+    .get<MyGroup>(`/mygroup`, { headers: { Authorization: token } })
     .then(res => res.data);
 };
 
-export const getPostsByDate = (date: string): Promise<GroupByDate> => {
+export const getPostsByDate = (date: string, token = ''): Promise<GroupByDate> => {
   return apiClient
     .get<GroupByDate>(`/mygroup/calendar/:${date}`, {
       headers: {
-        Authorization: TEST_TOKEN,
+        Authorization: token,
       },
     })
     .then(res => res.data);
 };
 
-export const createPost = (body: Obj): Promise<AxiosResponse<MyGroup>> => {
+export const createPost = (body: Obj, token = ''): Promise<AxiosResponse<MyGroup>> => {
   return apiClient.post<MyGroup>(`/mygroup/addpost`, body, {
-    headers: { Authorization: TEST_TOKEN },
+    headers: { Authorization: token },
   });
+};
+
+export const joinGroup = (token = ''): Promise<AxiosResponse<Obj>> => {
+  return apiClient.post(`/mygroup/join`, {}, { headers: { Authorization: token } });
 };
