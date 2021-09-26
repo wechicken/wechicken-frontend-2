ae926400cb5378af29bc8e16118ef613953388a6
positiveko <edie.spark1er@gmail.com>
2021-09-26 17:10:47 +0900

    feat: 404 custom page 작성

---
+++ pages/404.tsx
@@ -0,0 +1,62 @@
+import styled from '@emotion/styled';
+
+function Error(statusCode: string): JSX.Element {
+  console.log(
+    statusCode ? `An error ${statusCode} occurred on server` : 'An error occurred on client',
+  );
+
+  return (
+    <ErrorContainer>
+      <ErrorContents>
+        <div className="errorImg" />
+        <span className="errorText">404!</span>
+      </ErrorContents>
+    </ErrorContainer>
+  );
+}
+
+export default Error;
+
+const ErrorContainer = styled.div`
+  width: 100vw;
+  height: calc(100vh - 100px);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+`;
+
+const ErrorContents = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  height: 18.75rem;
+  min-height: 260px;
+
+  .errorImg {
+    width: 200px;
+    height: 200px;
+    border-radius: 50%;
+    background: url('/images/doghair.jpg');
+    background-position: center;
+    background-repeat: no-repeat;
+  }
+
+  .errorText {
+    font-size: 3.4375rem;
+    font-weight: 600;
+    font-family: ${({ theme }) => theme.fontTitle};
+    color: ${({ theme }) => theme.vermilion};
+    animation: blink-animation 0.8s steps(4, start) infinite alternate;
+  }
+
+  @keyframes blink-animation {
+    from {
+      visibility: visibility;
+    }
+    to {
+      visibility: hidden;
+    }
+  }
+`;
