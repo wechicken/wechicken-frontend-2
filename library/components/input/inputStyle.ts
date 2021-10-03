695903bf1ae3105d5dae96f84acf1505c4c03d43
positiveko <edie.spark1er@gmail.com>
2021-10-03 16:35:57 +0900

    fix: search page input event debounce 적용 및 design 수정

--- library/components/input/inputStyle.ts
+++ library/components/input/inputStyle.ts
@@ -15,38 +15,44 @@ export const InputBox = styled.div<{ width: string; size: string; search?: boole
   .nameBox {
     padding-left: 12px;
     padding-bottom: 2px;
+    ${({ search }) =>
+      search &&
+      css`
+        padding-left: 0;
+      `}
+  }
 
-    .type {
-      padding: 0 2px 6px 0;
-      font-weight: 500;
-      font-size: 15px;
-      color: ${({ theme }) => theme.textGrey};
-    }
+  .type {
+    padding: 0 2px 6px 0;
+    font-weight: 500;
+    font-size: 15px;
+    color: ${({ theme }) => theme.textGrey};
+  }
 
-    input {
-      width: ${({ width }) => width};
-      size: ${({ size }) => size};
-      ${({ size, search }) => search && `size: calc(${size} + 1rem)`};
-      border: none;
-      border-bottom: 1px solid rgba(0, 0, 0, 0.2);
-      outline: none;
-      cursor: text;
-      caret-color: ${({ theme }) => theme.vermilion};
-      ${({ search }) =>
-        search &&
-        css`
-          background-color: #ffffff1d;
-          backdrop-filter: blur(5px);
-        `}
-      ${({ search, theme }) => theme.sm`
+  input {
+    width: ${({ width }) => width};
+    size: ${({ size }) => size};
+    ${({ size, search }) => search && `size: calc(${size} + 1rem)`};
+    border: none;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+    outline: none;
+    cursor: text;
+    caret-color: ${({ theme }) => theme.vermilion};
+    ${({ search }) =>
+      search &&
+      css`
+        font-size: 2.5rem;
+        background-color: #ffffff1d;
+        backdrop-filter: blur(5px);
+      `}
+    ${({ search, theme }) => theme.sm`
         ${search && 'width: 100%'}
       `}
-    }
+  }
 
-    input::placeholder {
-      font-size: 14px;
-      opacity: 0.7;
-    }
+  input::placeholder {
+    font-size: 14px;
+    opacity: 0.7;
   }
 `;
 
